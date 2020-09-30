a10a745ac63685363cfbf034c65768f47fc120bd
Ahmed Osama <ahmed.osama.abbas@gmail.com>
2020-09-30 21:16:14 +0200

    Fix URL

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/AhmedOS/TestSPM/releases/download/9.1.7/Instabug.xcframework.zip",
+            url: "https://github.com/AhmedOS/TestSPM/releases/download/apple/Instabug.xcframework.zip",
             checksum: "665f00109192690a4f633c39152e3dbe6d971672f496fb940c320d8d6e7e294a")
     ]
 )
